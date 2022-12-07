8730c8e318d54f4a897793f7595d3ca480815723
mvpcouple@naver.com <mvpcouple@naver.com>
2022-12-07 16:24:40 +0900

    mongodb 저장하기 처음성공

--- server.js
+++ server.js
@@ -1,19 +1,54 @@
-const express =  require('express');
+const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 app.use(express.urlencoded({extended: true}));
+const bodyParser = require('body-parser');
 
-app.listen(8080, function(){
-  console.log('listening on 8080')
+var db;
+const MongoClient = require('mongodb').MongoClient;
+MongoClient.connect('mongodb+srv://skons:qwer1234@cluster0.nioo7sz.mongodb.net/todoapp?retryWrites=true&w=majority', function(에러, client){
+  if (에러) return console.log(에러);
+  //서버띄우는 코드 여기로 옮기기
+
+  db = client.db('todoapp');
+
+  db.collection('post').insertOne( {이름 : 'John', _id : 100} , function(에러, 결과){
+  console.log('저장완료'); 
+  });
+  app.listen('8080', function(){
+    console.log('listening on 8080')
+  });
 });
 
-app.get('/pet',function(요청, 응답){
-  응답.send('펫용품 쇼핑 사이트');
-});
+// const { MongoClient } = require("mongodb");
+// const username = encodeURIComponent("skons");
+// const password = encodeURIComponent("qwer1234");
+// const cluster = "cluster0";
+// const authSource = "<authSource>";
+// const authMechanism = "<authMechanism>";
 
-app.get('/beauty',function(요청, 응답){
-  응답.send('뷰티 쇼핑 사이트입니다.');
-});
+
+// let uri =
+//   `mongodb+srv://skons:<qwer1234>@cluster0.nioo7sz.mongodb.net/?retryWrites=true&w=majority`;
+// const client = new MongoClient(uri);
+// async function run() {
+//   try {
+//     await client.connect();
+//     const database = client.db("<dbName>");
+//     const ratings = database.collection("<collName>");
+//     const cursor = ratings.find();
+//     await cursor.forEach(doc => console.dir(doc));
+//   } finally {
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
+
+
+
+// app.listen(8080, function(){
+//   console.log('listening on 8080 : first')
+// });
+
 
 app.get('/',function(요청, 응답){
   응답.sendFile(__dirname + '/index.html')
