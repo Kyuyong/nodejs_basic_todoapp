c391b6d04fe176bdf0f54203e786d03e35a0481b
mvpcouple@naver.com <mvpcouple@naver.com>
2022-12-08 17:19:21 +0900

    todo 상세페이지 만들기

--- server.js
+++ server.js
@@ -63,3 +63,13 @@ app.delete('/delete', function(요청, 응답){
   })
 
 });
+
+// /detail/1 로 접속하면 detail1.ejs 보여줌 
+// /detail/2 로 접속하면 detail2.ejs 보여줌 
+
+app.get('/detail/:id', function(요청, 응답){
+  db.collection('post').findOne({_id: parseInt(요청.params.id)}, function(에러, 결과){
+    console.log(결과);
+    응답.render('detail.ejs', { data : 결과})
+  });
+});
