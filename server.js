9de9ae8c8143fa14ba11192540dbbd03b6bce4e1
mvpcouple@naver.com <mvpcouple@naver.com>
2022-12-08 11:34:39 +0900

    db post에서 등록된 내용 html에서 보여주기

--- server.js
+++ server.js
@@ -33,5 +33,11 @@ app.post('/add', function(요청, 응답){
 
 // /list로 get요청을 접속하면 HTML을 보여줌
 app.get('/list', function(요청, 응답){
-  응답.render('list.ejs');
+  
+  // DB에 저장된 post라는 collection안의 모든 데이터를 꺼내주세요 
+  db.collection('post').find().toArray(function(에러, 결과){
+    console.log(결과);
+    응답.render('list.ejs', {posts: 결과});
+  });  //모든 데이터 다 가져오기 
+
 });
