5ce4c05699c3dacdfa8fad41ce3f0247c72021a2
mvpcouple@naver.com <mvpcouple@naver.com>
2022-12-08 15:33:24 +0900

    todo 항목 id자동 생성, total 갯수 관리하기

--- server.js
+++ server.js
@@ -24,12 +24,24 @@ app.get('/write',function(요청, 응답){
 
 app.post('/add', function(요청, 응답){
   응답.send('전송완료');
-    console.log(요청.body.title);
-    console.log(요청.body.date);
-    db.collection('post').insertOne({제목:요청.body.title, 날짜:요청.body.date, }, function(){
+  db.collection('counter').findOne({name : '게시물갯수'}, function(에러, 결과){
+    console.log(결과.totalPost);
+    var 총게시물갯수 = 결과.totalPost;
+
+    db.collection('post').insertOne({_id: 총게시물갯수+1 ,제목:요청.body.title, 날짜:요청.body.date, }, function(){
       console.log('저장완료');
+      // counter라는 콜렉션 totalPost 항목도 1 증가 필요
+      // 수정할때는 operator를 써야함 
+      // operator에서 $set은 바꿔주세요. $inc : 기존값에 더해주세요.
+      db.collection('counter').updateOne({name:'게시물갯수'},{$inc:{totalPost:1}},function(에러, 결과){
+        if(에러) return console.log(에러);
+      });
     })
+  
+  });
 });
+
+
 
 // /list로 get요청을 접속하면 HTML을 보여줌
 app.get('/list', function(요청, 응답){
