9f396605662e78f2c6330d3a5bc49dfbfdecbdfc
조규용 <mvpcouple@naver.com>
2022-12-22 15:09:03 +0900

    채팅방 만들기

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 app.set('view engine', 'ejs');
 const methodOverride = require('method-override');
 app.use(methodOverride('_method'));
+const { ObjectId } = require('mongodb');
 
 
 app.use('/public', express.static('public'));
@@ -278,3 +279,37 @@ app.get('/image/:imageName', function(요청,응답){
   응답.sendFile( __dirname + '/public/image/' + 요청.params.imageName)
 });
 
+app.post('/chatroom', 로그인했니,function(요청,응답){
+  var 저장할거 = {
+    title : '무슨채팅방',
+    member : [ObjectId(요청.body.당한사람id), 요청.user._id],
+    date : new Date()
+  };
+  db.collection('chatroom').insertOne(저장할거).then((결과)=>{
+    응답.send('성공');
+  });
+});
+
+app.get('/chat',로그인했니 ,function(요청,응답){
+  db.collection('chatroom').find({member:요청.user._id }).toArray().then((결과)=>{
+    응답.render('chat.ejs', {data: 결과});
+  });
+
+});
+
+app.post('/message',로그인했니 ,function(요청,응답){
+  
+  var 저장할거 = {
+    parent : 요청.body.parent,
+    content : 요청.body.content,
+    userid : 요청.user._id,
+    date : new Date(),
+
+  };
+  
+  db.collection('message').insertOne(저장할거).then(()=>{
+    console.log('성공');
+    응답.send('DB저장성공')
+  });
+
+});
